74977b656cd4d2a4d2fe345ae17f3f37bdd45e99
Edvaldo Santana <edvaldosantana@Mac-mini-de-Edvaldo.local>
2020-12-09 22:15:18 -0300

    Trabalhando com futuro

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
-void main() => runApp(MyApp());
+const request = 'https://api.hgbrasil.com/finance?format=json&key=424e3f5d';
+void main() async {
+  print(await getData());
 
-class MyApp extends StatelessWidget {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '\$ Conversor de Moedas \$',
+          style: TextStyle(color: Colors.yellow, fontSize: 25),
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+        centerTitle: true,
       ),
     );
   }
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
 }
